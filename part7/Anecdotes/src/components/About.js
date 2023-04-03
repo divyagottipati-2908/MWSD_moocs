2bf7289c5433041c237db666856fe78417c2c3c6
GOTTIPATI DIVYA <110450411+divyagottipati-2908@users.noreply.github.com>
2023-04-03 15:01:41 +0530

    Create About.js

---
+++ part7/Anecdotes/src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h2>About Anecdote App</h2>
+    <p>According to the Wikipedia:</p>
+
+    <em>
+      An anecdote is a brief, revealing account of an individual person or an
+      incident
+    </em>
+
+    <p>
+      Software engineering is full of excellent anecdotes
+    </p>
+  </div>
+)
+
+export default About
