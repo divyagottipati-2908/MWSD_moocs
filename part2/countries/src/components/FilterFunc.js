579ea8d7acd6b2943e5912bbb05e26dfc3ce2d8c
GOTTIPATI DIVYA <110450411+divyagottipati-2908@users.noreply.github.com>
2023-04-03 14:32:09 +0530

    Delete part2/countries directory

--- part2/countries/src/components/FilterFunc.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const FilterFunc = ({val, Change}) =>
-    <div>
-        find countries <input value={val} onChange={Change} />
-    </div>
-
-export default FilterFunc
